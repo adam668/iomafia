36212b3df129d4cf02b1b47e4bbbbea941bef796
Hugo Rydman <carlhugo.rydman@gmail.com>
2021-01-30 23:43:25 +0100

    Started on express server!

--- src/Startpage/Startpage.tsx
+++ src/Startpage/Startpage.tsx
@@ -1,8 +1,7 @@
+import Button from '../Button/Button'
 import './Startpage.css'
-import { useState } from 'react'
 
 export default function Startpage() {
-
 
     return <section>
         <div id="title">
@@ -12,16 +11,17 @@ export default function Startpage() {
             </div>
         </div>
         <div id="main">
-            <div id="buttons">
-                <button>Start</button>
-                <button>Profile</button>
-                <button>Options</button>
-                <button>Friends</button>
-            </div>
             <h1><code>MAFIA.IO</code></h1>
+            <div id="buttons">
+                <Button title="Start" action={ () => undefined }/>
+                <Button title="Profile" action={ () => undefined }/>
+                <Button title="Friends" action={ () => undefined }/>
+                <Button title="Options" action={ () => undefined }/>
+                <Button className="fas fa-cogs"/>
+            </div>
         </div>
-        <div id="contacts">
-            <a href="https://discord.gg/8ckkwTYhp6" target="_blank" rel="noreferrer"><i className="fab fa-discord fa-3x" ></i></a>
+        <div id="contacts" style={{position: "absolute", right: "15px", bottom: "15px"}}>
+            <a style={{color: "white"}} href="https://discord.gg/8ckkwTYhp6" target="_blank" rel="noreferrer"><i style={{marginLeft: "15px"}} className="fab fa-discord fa-3x" ></i></a>
         </div>
     </section>
 }
