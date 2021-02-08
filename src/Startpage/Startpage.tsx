8b207dca4cc9c3ec27149d0297d3c8514f964e08
Hugo Rydman <carlhugo.rydman@gmail.com>
2021-02-08 21:39:49 +0100

    Changed title style, trying some stuff out.

--- src/Startpage/Startpage.tsx
+++ src/Startpage/Startpage.tsx
@@ -1,3 +1,4 @@
+'use strict'
 /* eslint-disable react/no-direct-mutation-state */
 import Button from '../Button/Button'
 import './Startpage.css'
@@ -5,56 +6,53 @@ import Dialog from '../Dialog/dialog'
 import React, {Component} from 'react';
 
 export default class Startpage extends Component {
-    
-        state = {
-          isModalOpen: false,
-          max: 100, 
-          min: 0, 
-          value: 0
-        }
-      toggleModal = () => {
+
+    state = {
+        isModalOpen: false,
+        max: 100, 
+        min: 0, 
+        value: 0
+    }
+    toggleModal = () => {
         const { isModalOpen } = this.state;
         this.setState({ isModalOpen: !isModalOpen });
-      }
-    
-      render() {
-          return (
-              <React.Fragment>
-<section>
-        <div id="title">
-            <div id="login-buttons">
-                <button id="login">Login</button>
-                <button id="register">Register</button>
-            </div>
-        </div>
-        <div id="main">
-            <h1><code>MAFIA.IO</code></h1>
-            <div id="buttons">
-                <Button title="Start" action={ () => undefined }/>
-                <Button title="Profile" action={ () => undefined }/>
-                <Button title="Friends" action={ () => undefined }/>
-                <div>
-                <Button title="Options" action={() => this.toggleModal()}/>
-              
-                <Dialog isOpen={this.state.isModalOpen} onClose={this.toggleModal}>
-            <div>
-          Settings
-          </div>
-          <div id='SettingsText'>
-              Music: <br/>
-              <input type="range" min={this.state.min} max={this.state.max} value={this.state.value} 
-             onChange={() => console.log('hi')} className="slider" id="myRange"></input>
-          </div>
-        </Dialog>
-   </div>
-            </div>
-        </div>
-        <div id="contacts" style={{position: "absolute", right: "15px", bottom: "15px"}}>
-            <a style={{color: "white"}} href="https://discord.gg/8ckkwTYhp6" target="_blank" rel="noreferrer" id='discord'><i style={{marginLeft: "15px"}} className="fab fa-discord fa-3x" ></i></a>
-        </div>
-    </section>
-    </React.Fragment>
-          )
-      }
-     
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <section>
+                    <div id="header">
+                        <div id="title"><h1>Little Fantasy</h1></div>
+                        <div id="top-buttons">
+                            <button id="login">Login</button>
+                        </div>
+                    </div>
+                    <div id="main">
+                        <div id="buttons">
+                            <Button title="Start" action={ () => undefined }/>
+                            <Button title="Profile" action={ () => undefined }/>
+                            <Button title="Friends" action={ () => undefined }/>
+                            <div>
+                                <Button title="Options" action={() => this.toggleModal()}/>
+                                <Dialog style={{transition: '0.23s'}} isOpen={this.state.isModalOpen} onClose={this.toggleModal}>
+                                    <div>
+                                        Settings
+                                    </div>
+                                    <div id='SettingsText'>
+                                        Music: <br/>
+                                        <input type="range" min={this.state.min} max={this.state.max} value={this.state.value} 
+                                        onChange={() => console.log('hi')} className="slider" id="myRange"></input>
+                                    </div>
+                                </Dialog>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="contacts" style={{position: "absolute", right: "15px", bottom: "15px"}}>
+                        <a style={{color: "white"}} href="https://discord.gg/8ckkwTYhp6" target="_blank" rel="noreferrer" id='discord'><i style={{marginLeft: "15px"}} className="fab fa-discord fa-3x" ></i></a>
+                    </div>
+                </section>
+            </React.Fragment>
+        )
+    }
 }
