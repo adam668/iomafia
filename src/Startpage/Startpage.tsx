69a2d97b4f3b1350b61e4c2f6f910ecedf88db6a
adam668 <eladdouliadam3@gmail.com>
2021-02-10 20:50:39 -0500

    basic name prompt

--- src/Startpage/Startpage.tsx
+++ src/Startpage/Startpage.tsx
@@ -3,14 +3,14 @@ import Button from '../Button/Button'
 import './Startpage.css'
 import Dialog from '../Dialog/dialog'
 import React, {Component} from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import Register from '../pages/register'
 
 export default class Startpage extends Component {
 
     state = {
         isModalOpen: false,
-        max: 100, 
-        min: 0, 
-        value: 0
+        userName: null
     }
     toggleModal = () => {
         const { isModalOpen } = this.state;
@@ -19,20 +19,29 @@ export default class Startpage extends Component {
 
     render() {
         return (
+            <Router>
             <React.Fragment>
                 <section>
+                <Route path="/register" component={Register} ></Route>  
                     <div id="header">
                         <div id="title">
-                            <h1>Little Fantasy</h1>
+                            <h1>Little Fantasy</h1> <br/>
+                            <h2>Name (temporary): {this.state.userName ?? "None"} </h2>
                             <div id="top-buttons">
                                 <button id="login">Login</button>
-                                <button id="register">Register</button>
+                                <button id="register" onClick={() => {window.location.href = 'localhost:5000/register'}}>Register</button>
                             </div>
                         </div>
                     </div>
                     <div id="main">
                         <div id="buttons">
-                            <Button title="Start" action={ () => undefined }/>
+                            <Button title="Start" action={ () => {
+                                let newName = prompt('What do you want your name to be?')
+                                while (newName !== null && newName.trim() !== "" && newName.length >= 10) {
+                                    newName = prompt("Enter your name (needs to be below or equal to 10):");
+                                  }
+                                this.setState({userName: newName})
+                            } }/>
                             <Button title="Profile" action={ () => undefined }/>
                             <Button title="Friends" action={ () => undefined }/>
                             <div>
@@ -43,8 +52,7 @@ export default class Startpage extends Component {
                                     </div>
                                     <div id='SettingsText'>
                                         Music: <br/>
-                                        <input type="range" min={this.state.min} max={this.state.max} value={this.state.value} 
-                                        onChange={() => console.log('hi')} className="slider" id="myRange"></input>
+                                       
                                     </div>
                                 </Dialog>
                             </div>
@@ -53,8 +61,10 @@ export default class Startpage extends Component {
                     <div id="contacts" style={{position: "absolute", right: "15px", bottom: "15px"}}>
                         <a style={{color: "white"}} href="https://discord.gg/8ckkwTYhp6" target="_blank" rel="noreferrer" id='discord'><i style={{marginLeft: "15px"}} className="fab fa-discord fa-3x" ></i></a>
                     </div>
-                </section>
+                </section> 
+                     
             </React.Fragment>
+            </Router>
         )
     }
 }
---
+++ src/pages/register.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React, { Component } from "react";
+
+
+export default class register extends Component {
+  
+    render() {
+        return (
+            <React.Fragment>
+            <div>
+                hi
+            </div>
+            </React.Fragment>
+        )
+    }
+  
+}
